93e71bcc8b9f03e486d9332099fd7edf8c38d40a
ErickErraez <edg.erraez@yavirac.edu.ec>
2018-11-25 22:04:30 -0600

    ok

--- IonChat/src/app/app.module.ts
+++ IonChat/src/app/app.module.ts
@@ -13,6 +13,15 @@ import { NavbarComponent } from './login/navbar/navbar.component';
 import { BodyComponent } from './login/body/body.component';
 import { FooterComponent } from './login/footer/footer.component';
 import { MuroComponent } from './muro/muro.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'friends', component: FriendsComponent }
+
+];
 
 @NgModule({
   declarations: [
@@ -30,6 +39,7 @@ import { MuroComponent } from './muro/muro.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
