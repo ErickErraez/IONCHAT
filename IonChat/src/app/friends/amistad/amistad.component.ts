3ed9a3730426377ee6550657f6f7740a16ccc6bb
mauricioencalada <dma.encalada@yavirac.edu.ec>
2018-11-26 21:53:06 -0500

    'profile'

---
+++ IonChat/src/app/friends/amistad/amistad.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amistad',
+  templateUrl: './amistad.component.html',
+  styleUrls: ['./amistad.component.css']
+})
+export class AmistadComponent implements OnInit {
+
+
+  
+constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ IonChat/src/app/profile/imgper/imgper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imgper',
+  templateUrl: './imgper.component.html',
+  styleUrls: ['./imgper.component.css']
+})
+export class ImgperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
